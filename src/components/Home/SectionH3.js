9ceda010e3197de8b12fe6b2e7beb22836d0b994
FatbardhMulaku <fatbardh.mulaku@gmail.com>
2020-09-15 23:56:56 +0200

    first commit

--- src/components/Home/SectionH3.js
+++ src/components/Home/SectionH3.js
@@ -4,7 +4,7 @@ import "./SectionH3.css";
 function Teknologies() {
     return (
         <div  className="teknologies">
-            <h1>Teknologies that<br/> KosovaOutsource use</h1>
+            <h1>Technologies that<br/> KosovaOutsource use</h1>
             <div data-aos="fade-up" data-aos-once="false" data-aos-duration="3500" className="teknologies__logos">
                 <div className="tlogo logo-java"></div>
                 <div className="tlogo logo-html"></div>
