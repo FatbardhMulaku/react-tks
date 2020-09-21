33ba8c429e30b3cb9627a5cb9c43d8334fb2c6cd
Albrim Paqarizi <albrimpa@gmail.com>
2020-09-21 04:48:07 +0200

    Update footer

--- src/components/UI/SocialMedia/SocialMedia.js
+++ src/components/UI/SocialMedia/SocialMedia.js
@@ -4,6 +4,7 @@ import {
   FaTwitter,
   FaLinkedinIn,
   FaYoutube,
+  FaBehance,
 } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 
@@ -12,56 +13,80 @@ import "./SocialMedia.css";
 const SocialMedia = () => {
   return (
     <div className="SocalMedia">
-      <a
-        rel="noopener noreferrer"
-        target="_blank"
-        href="https://www.instagram.com"
-      >
-        <span>
-          <RiInstagramFill />
-        </span>
-        Instagram
-      </a>
-      <a
-        rel="noopener noreferrer"
-        target="_blank"
-        href="https://www.twitter.com"
-      >
-        <span>
-          <FaTwitter />
-        </span>
-        Twitter
-      </a>
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.facebook.com"
-      >
-        <span>
-          <FaFacebookF />
-        </span>
-        Facebook
-      </a>
-      <a
-        rel="noopener noreferrer"
-        target="_blank"
-        href="https://www.linkedin.com"
-      >
-        <span>
-          <FaLinkedinIn />
-        </span>
-        LinkedIn
-      </a>
-      <a
-        rel="noopener noreferrer"
-        target="_blank"
-        href="https://www.youtube.com"
-      >
-        <span>
-          <FaYoutube />
-        </span>
-        Youtube
-      </a>
+      <div className="row">
+        <div className="col-sm-6 col-md-4 col-xl-2 my-2">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.instagram.com"
+          >
+            <span>
+              <RiInstagramFill />
+            </span>
+            Instagram
+          </a>
+        </div>
+        <div className="col-sm-6 col-md-4 col-xl-2 my-2">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.twitter.com"
+          >
+            <span>
+              <FaTwitter />
+            </span>
+            Twitter
+          </a>
+        </div>
+        <div className="col-sm-6 col-md-4 col-xl-2 my-2">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com"
+          >
+            <span>
+              <FaLinkedinIn />
+            </span>
+            LinkedIn
+          </a>
+        </div>
+        <div className="col-sm-6 col-md-4 col-xl-2 my-2">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.behance.net"
+          >
+            <span>
+              <FaBehance />
+            </span>
+            Behance
+          </a>
+        </div>
+        <div className="col-sm-6 col-md-4 col-xl-2 my-2">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com"
+          >
+            <span>
+              <FaFacebookF />
+            </span>
+            Facebook
+          </a>
+        </div>
+        <div className="col-sm-6 col-md-4 col-xl-2 my-2">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.youtube.com"
+          >
+            <span>
+              <FaYoutube />
+            </span>
+            Youtube
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
