a83e1782882c6a2d5239c706da33b515dce09ce1
Albrim Paqarizi <albrimpa@gmail.com>
2020-09-28 01:57:19 +0200

    Update our agency page

--- src/components/OurAgency/SectionOA1.js
+++ src/components/OurAgency/SectionOA1.js
@@ -7,7 +7,7 @@ import TextGrid from "../Shared/TextGrid/TextGrid";
 const SectionOA1 = () => {
   return (
     <LayoutGrid section="sectionOA1" row="sectionOA1_row">
-      <ImgGrid img="img_ourAgency_1" />
+      <ImgGrid img="img_ourAgency_1 chevron" />
       <TextGrid textCss="sectionOA1_text col_white" tr="ourAgency.section_1" />
     </LayoutGrid>
   );
