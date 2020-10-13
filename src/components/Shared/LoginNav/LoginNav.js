76dc347a2ac94b90f0f24ecc615d06415020ab83
FatbardhMulaku <fatbardh.mulaku@gmail.com>
2020-10-13 22:22:15 +0200

    updated login/sign-up nav

--- src/components/Shared/LoginNav/LoginNav.js
+++ src/components/Shared/LoginNav/LoginNav.js
@@ -4,6 +4,29 @@ import { useTranslation } from "react-i18next";
 import LoginModal from "../../UI/Modal/Login";
 import SignUpModal from "../../UI/Modal/SignUp";
 import { useToggle } from "../../../Hooks/useToggle";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    klasa1:{
+      backgroundColor: 'white',
+      padding: '4px 10px',
+      color: 'black',
+      fontWeight: 'bold',
+      cursor:'pointer',
+      borderRadius: '6px',
+      border: '1px solid black'
+    },
+    klasa2:{
+      backgroundColor: 'white',
+      padding: '4px 10px',
+      color: '#006FFF',
+      fontWeight: 'bold',
+      marginLeft:"10px",
+      cursor:'pointer',
+      borderRadius: '6px',
+      border: '1px solid #006FFF'
+    }
+});
 
 const LoginNav = (props) => {
   const { t } = useTranslation();
@@ -14,17 +37,16 @@ const LoginNav = (props) => {
     setlogOpen();
     setsignOpen();
   };
-
-  let pHover = props.trigger ? "black-p" : "white-p";
+  
+  const classes = useStyles();
   return (
     <div>
       <ul className="nav-login p-0 my-auto">
-        <li onClick={setlogOpen} className={pHover}>
+        <li onClick={setlogOpen} className={classes.klasa1}>
           <p>{t("loginBtn")}</p>
         </li>
-        <hr className={props.trigger ? "bg_black" : "bg_white"} />
 
-        <li onClick={setsignOpen} className={pHover}>
+        <li onClick={setsignOpen} className={classes.klasa2}>
           <p>{t("signupBtn")}</p>
         </li>
       </ul>
