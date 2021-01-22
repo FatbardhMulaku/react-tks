b20ad8874bf67b417d7a1302fb5ecfdd55fa3d2d
FatbardhMulaku <fatbardh.mulaku@gmail.com>
2021-01-22 20:55:33 +0100

    updated header menu buttons

--- src/components/Shared/LoginNav/LoginNav.js
+++ src/components/Shared/LoginNav/LoginNav.js
@@ -5,26 +5,66 @@ import LoginModal from "../../UI/Modal/Login";
 import SignUpModal from "../../UI/Modal/SignUp";
 import { useToggle } from "../../../Hooks/useToggle";
 import { makeStyles } from '@material-ui/core/styles';
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 
 const useStyles = makeStyles((theme) => ({
+    klasa1a:{
+      backgroundColor: 'transparent',
+      padding: '6px 10px',
+      color: 'white',
+      fontWeight: 'bold',
+      cursor:'pointer',
+      borderRadius: '6px',
+      border: '1px solid white',
+      "&:hover": {
+        border: "1px solid white",
+        backgroundColor: "white",
+        color: "black",
+      },
+    },
     klasa1:{
-      backgroundColor: 'white',
-      padding: '4px 10px',
+      backgroundColor: 'transparent',
+      padding: '6px 10px',
       color: 'black',
       fontWeight: 'bold',
       cursor:'pointer',
       borderRadius: '6px',
-      border: '1px solid black'
+      border: '1px solid black',
+      "&:hover": {
+        border: "1px solid black",
+        backgroundColor: "black",
+        color: "white",
+      },
     },
-    klasa2:{
+    klasa2a:{
       backgroundColor: 'white',
-      padding: '4px 10px',
-      color: '#006FFF',
+      padding: '6px 10px',
+      color: '#000',
       fontWeight: 'bold',
       marginLeft:"10px",
       cursor:'pointer',
       borderRadius: '6px',
-      border: '1px solid #006FFF'
+      border: '1px solid #fff',
+      "&:hover": {
+        border: "1px solid #006FFF",
+        backgroundColor: "#006FFF",
+        color: "white",
+      },
+    },
+    klasa2:{
+      backgroundColor: '#006FFF',
+      padding: '6px 10px',
+      color: '#fff',
+      fontWeight: 'bold',
+      marginLeft:"10px",
+      cursor:'pointer',
+      borderRadius: '6px',
+      border: '1px solid #006FFF',
+      "&:hover": {
+        border: "1px solid #006FFF",
+        backgroundColor: "transparent",
+        color: "#006FFF",
+      },
     },
     [theme.breakpoints.down('md')]: {
       backgroundColor: 'red',
@@ -42,15 +82,24 @@ const LoginNav = (props) => {
     setsignOpen();
   };
   
-  const classes = useStyles();
+  const classes = useStyles(props);
+
+  const { windowp } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 50,
+    target: windowp ? window() : undefined,
+  });
+
   return (
     <div>
       <ul className="nav-login p-0 my-auto">
-        <li onClick={setlogOpen} className={classes.klasa1}>
+        <li onClick={setlogOpen} className={trigger ? classes.klasa1 : classes.klasa1a}>
           <p>{t("loginBtn")}</p>
         </li>
 
-        <li onClick={setsignOpen} className={classes.klasa2}>
+        <li onClick={setsignOpen} className={trigger ? classes.klasa2 :classes.klasa2a}>
           <p>{t("signupBtn")}</p>
         </li>
       </ul>
